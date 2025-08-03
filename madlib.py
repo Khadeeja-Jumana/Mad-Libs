184abdc5eecdca23a7e6464bc3919812aaa56063
Khadeeja Jumana <khadeejajumana3@gmail.com>
2025-08-03 14:10:02 +0530

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -6,10 +6,11 @@ adj2=input("Adjective:")
 verb=input("Verb ending with 'ing':")
 
 
-madlib="One {} evening, Jumana sat by her window, feeling rather {} after a long day. She held her favorite {}, hoping it would bring her comfort. \
+madlib="One {} evening, Lilly sat by her window, feeling rather {} after a long day. She held her favorite {}, hoping it would bring her comfort. \
 Suddenly, she heard a strange sound coming from the backyard. Curious, she stepped outside and was shocked to see a {} standing next to the garden hose. \
 But this was no ordinary animal — it had a {} backpack on its back and was calmly {} like it did this every day. \
-Jumana couldn’t believe her eyes. She slowly walked closer and whispered, 'Hello?' The animal turned, gave her a little bow, and tossed her a glowing pebble. \
+Lilly couldn’t believe her eyes. She slowly walked closer and whispered, 'Hello?' The animal turned, gave her a little bow, and tossed her a glowing pebble. \
 Before she could react, the world around her shifted. Trees floated in the sky, the grass turned into jelly, and time itself seemed to slow down. \
 All because of one unexpected encounter with a mysterious visitor who came without warning... and changed everything.".format(adj1,emotion,noun,animal,adj2,verb)
+
 print(madlib)
